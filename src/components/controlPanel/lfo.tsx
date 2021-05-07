5b6aec0daf2ec5d43abceeaee80586f9080ec57d
6sRyuSK <zikkyou19@gmail.com>
2021-05-07 21:14:40 +0900

    add lfo style

--- src/components/controlPanel/lfo.tsx
+++ src/components/controlPanel/lfo.tsx
@@ -1,8 +1,9 @@
 import RotaryEncoder from '../atoms/rotaryEncoder'
+import styles from '../../styles/controlPanel/lfo.module.css'
 
 const LFO = () => {
   return (
-    <div>
+    <div className={styles.lfo}>
       <span>LFO</span>
       <RotaryEncoder label="Rate"></RotaryEncoder>
       <RotaryEncoder label="Depth"></RotaryEncoder>
