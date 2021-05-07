ce6b4f5c685c64d7ad48c84f094210a8ea6f26d7
6sRyuSK <zikkyou19@gmail.com>
2021-05-07 20:01:50 +0900

    可動域制限を持たせた

--- src/components/atoms/rotaryEncoder.tsx
+++ src/components/atoms/rotaryEncoder.tsx
@@ -3,6 +3,7 @@ import styles from '../../styles/atoms/rotaryEncoder.module.css'
 
 type props = {
   size?: number
+  limit?: [number, number]
 }
 
 const RotaryEncoder: React.FC<props> = (props: props) => {
@@ -14,9 +15,11 @@ const RotaryEncoder: React.FC<props> = (props: props) => {
     const Elm = document.getElementsByClassName(
       styles.rotaryEncoder
     )[0] as HTMLDivElement
-    let rotate = 0
+    let rotate = props.limit![0]
+    Elm.style.transform = `rotate(${rotate}deg)`
     Elm.onwheel = (e) => {
       rotate += Math.sign(e.deltaY) * 10
+      rotate = Math.min(Math.max(props.limit![0], rotate), props.limit![1])
       console.log(rotate)
       Elm.style.transform = `rotate(${rotate}deg)`
     }
@@ -30,6 +33,7 @@ const RotaryEncoder: React.FC<props> = (props: props) => {
 
 RotaryEncoder.defaultProps = {
   size: 32,
+  limit: [-140, 140],
 }
 
 export default RotaryEncoder
