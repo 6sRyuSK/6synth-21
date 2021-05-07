c022964bb0d2c18b9db0564b2dbdc28c3c57a5ac
6sRyuSK <zikkyou19@gmail.com>
2021-05-07 19:51:37 +0900

    マウスホイールで操作できるようにした

--- src/components/atoms/rotaryEncoder.tsx
+++ src/components/atoms/rotaryEncoder.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react'
 import styles from '../../styles/atoms/rotaryEncoder.module.css'
 
 type props = {
@@ -9,6 +10,17 @@ const RotaryEncoder: React.FC<props> = (props: props) => {
     width: props.size + 'px',
     height: props.size + 'px',
   }
+  useEffect(() => {
+    const Elm = document.getElementsByClassName(
+      styles.rotaryEncoder
+    )[0] as HTMLDivElement
+    let rotate = 0
+    Elm.onwheel = (e) => {
+      rotate += Math.sign(e.deltaY) * 10
+      console.log(rotate)
+      Elm.style.transform = `rotate(${rotate}deg)`
+    }
+  })
   return (
     <div style={style} className={styles.rotaryEncoder}>
       <div className={styles.dot}></div>
